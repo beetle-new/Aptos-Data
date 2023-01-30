cfb279762ea54b54e58b803f6822dadbef859723
beetle-new <121641549+beetle-new@users.noreply.github.com>
2023-01-29 21:52:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import pandas as pd
 import streamlit as st
 
+
 def query_api(query):
     url = "https://indexer.mainnet.aptoslabs.com/v1/graphql"
     headers = {
@@ -16,33 +17,34 @@ def query_api(query):
     else:
         return None
 
-owner_address = st.text_input("Enter an owner_address:", "0xc739507214d0e1bf9795485299d709e00024e92f7c0d055a4c2c39717882bdfd")
-
 query = """
-  MyQuery {
-    coin_activities(
-      where: {owner_address: {_eq: """ + f'"{owner_address}"' + """}, is_transaction_success: {_eq: true}, activity_type: {_neq: "0x1::aptos_coin::GasFeeEvent"}}
-      order_by: {transaction_timestamp: desc}
-    ) {
-      transaction_timestamp
-      owner_address
-      amount
-      activity_type
-      coin_type
-    }
+query MyQuery {
+  coin_activities(
+    where: {owner_address: {_eq: "0xc739507214d0e1bf9795485299d709e00024e92f7c0d055a4c2c39717882bdfd"}, is_transaction_success: {_eq: true}, activity_type: {_neq: "0x1::aptos_coin::GasFeeEvent"}}
+    order_by: {transaction_timestamp: desc}
+  ) {
+    transaction_timestamp
+    owner_address
+    amount
+    activity_type
+    coin_type
   }
-"""
+}
 
+
+"""
 
 data = query_api(query)
 df = pd.DataFrame(data['data']['coin_activities'])
+df = df.rename(columns={"transaction_timestamp": "Date",
+                        "owner_address": "Address",
+                        "amount": "amount"})
 
-    
-df['amount'] = round((df['amount'] / 100000000),2)
+df['amount'] = round(df['amount'] / 100000000,2)
 df['amount'] = df['amount'].apply(lambda x: "{:,.2f}".format(x))
 df['coin_type'] = df['coin_type'].str.split("::").str[-1]
 df['coin_type'] = df['coin_type'].str.rsplit("Event", 1).str[0]
 df['activity_type'] = df['activity_type'].str.split("::").str[-1]
 df['activity_type'] = df['activity_type'].str.rsplit("Event", 1).str[0]
-df = df.sort_values(by='transaction_timestamp', ascending=False)
-st.write(df, width=800)
+st.write(df)
+

a15801e927b703413e9b7979f362a16c8671756f
beetle-new <121641549+beetle-new@users.noreply.github.com>
2023-01-29 20:35:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#########################  FETCHING CURRENT BALANCES FOR APTOS ACCOUNTS ##############################     
-
 import requests
 import pandas as pd
 import streamlit as st
@@ -18,18 +16,22 @@ def query_api(query):
     else:
         return None
 
-query = """
-query MyQuery {
+# Add user input for the filter owner_address
+owner_address = st.text_input("Enter an owner_address:", "0xc739507214d0e1bf9795485299d709e00024e92f7c0d055a4c2c39717882bdfd")
+
+# Replace the fixed filter value with the user input
+query = f"""
+query MyQuery {{
   current_coin_balances(
-    where: {owner_address: {_eq: "0xc739507214d0e1bf9795485299d709e00024e92f7c0d055a4c2c39717882bdfd"}}
-  ) {
+    where: {{owner_address: {{_eq: "{owner_address}"}}}}
+  ) {{
     owner_address
     amount
-    coin_info {
+    coin_info {{
       symbol
-    }
-  }
-}
+    }}
+  }}
+}}
 """
 
 data = query_api(query)
@@ -45,33 +47,18 @@ df['amount'] = df['amount'].apply(lambda x: "{:,.2f}".format(x))
 st.write(df)
 
 
-def query_api(query):
-    url = "https://indexer.mainnet.aptoslabs.com/v1/graphql"
-    headers = {
-        "Content-Type": "application/json"
-    }
-    payload = {
-        "query": query
-    }
-    response = requests.post(url, json=payload, headers=headers)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
-
-query = """
-query MyQuery {
+# Replace the fixed filter value with the user input
+query = f"""
+query MyQuery {{
   coin_activities(
-    where: {owner_address: {_eq: "0xc739507214d0e1bf9795485299d709e00024e92f7c0d055a4c2c39717882bdfd"}, is_transaction_success: {_eq: true}}
-  ) {
+    where: {{owner_address: {{_eq: "{owner_address}"}}, is_transaction_success: {{_eq: true}}}}
+  ) {{
     transaction_timestamp
     owner_address
     amount
     activity_type
-  }
-}
-
-
+  }}
+}}
 """
 
 data = query_api(query)
@@ -79,6 +66,32 @@ df = pd.DataFrame(data['data']['coin_activities'])
 df = df.rename(columns={"transaction_timestamp": "Date",
                         "owner_address": "Address",
                         "amount": "amount"})
+
+df['amount'] = round(df['amount'] / 100000000,2)
+df['amount'] = df['amount'].apply(lambda x: "{:,.2f}".format(x))
+df['activity_type'] = df['activity_type'].str.split("::").str[-1]
+df['activity_type'] = df['activity_type'].str.rsplit("Event", 1).str[0]
+
+owner_address = st.text_input("Enter the owner address: ")
+
+query = f"""
+query MyQuery {{
+coin_activities(
+where: {{owner_address: {{_eq: "{owner_address}"}}, is_transaction_success: {{_eq: true}}}}
+) {{
+transaction_timestamp
+owner_address
+amount
+activity_type
+}}
+}}
+"""
+
+data = query_api(query)
+df = pd.DataFrame(data['data']['coin_activities'])
+df = df.rename(columns={"transaction_timestamp": "Date",
+"owner_address": "Address",
+"amount": "amount"})
 
 df['amount'] = round(df['amount'] / 100000000,2)
 df['amount'] = df['amount'].apply(lambda x: "{:,.2f}".format(x))

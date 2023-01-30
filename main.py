66a14d088e93c5754cc08a3feeb75d6f73b217ef
beetle-new <121641549+beetle-new@users.noreply.github.com>
2023-01-30 09:43:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -16,13 +16,14 @@ def query_api(query):
         return response.json()
     else:
         return None
-
+owner_address = "0xc739507214d0e1bf9795485299d709e00024e92f7c0d055a4c2c39717882bdfd"
+owner_address = st.text_input("Enter an owner address:", value=owner_address)
 
 def get_data(owner_address):
     query = f"""
     query MyQuery {{
       coin_activities(
-         where: {{owner_address: {{_eq: "{owner_address}"}}, is_transaction_success: {{_eq: true}}, activity_type: {{_neq: "0x1::aptos_coin::GasFeeEvent"}}}}
+         where: {{owner_address: {_eq: "%s"}}, is_transaction_success: {{_eq: true}}, activity_type: {{_neq: "0x1::aptos_coin::GasFeeEvent"}}}}
          order_by: {{transaction_timestamp: desc}}
       ) {{
         transaction_timestamp
@@ -31,7 +32,7 @@ def get_data(owner_address):
         coin_type
       }}
     }}
-    """
+    """ % owner_address
     data = query_api(query)
     df = pd.DataFrame(data['data']['coin_activities'])
     df = df.rename(columns={"transaction_timestamp": "Date",
@@ -44,9 +45,6 @@ def get_data(owner_address):
     df['activity_type'] = df['activity_type'].str.split("::").str[-1]
     df['activity_type'] = df['activity_type'].str.rsplit("Event", 1).str[0]
     return df
-
-owner_address = "0xc739507214d0e1bf9795485299d709e00024e92f7c0d055a4c2c39717882bdfd"
-owner_address = st.text_input("Enter an owner address:", value=owner_address)
 
 query = """
 query MyQuery {

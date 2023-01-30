e1302f7517ca57dc8dcf0a0deaa327dbfb4eb308
beetle-new <121641549+beetle-new@users.noreply.github.com>
2023-01-29 21:40:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -19,17 +19,17 @@ def query_api(query):
 owner_address = st.text_input("Enter an owner_address:", "0xc739507214d0e1bf9795485299d709e00024e92f7c0d055a4c2c39717882bdfd")
 
 query = f"""
-query MyQuery {{
+query MyQuery {
   coin_activities(
-    where: {{owner_address: {{_eq: "{owner_address}"}}, is_transaction_success: {{_eq: true}}}}
-  ) {{
+    where: {owner_address: {_eq: "0xc739507214d0e1bf9795485299d709e00024e92f7c0d055a4c2c39717882bdfd"}, is_transaction_success: {_eq: true}, activity_type: {_neq: "0x1::aptos_coin::GasFeeEvent"}}
+    order_by: {transaction_timestamp: desc}
+  ) {
     transaction_timestamp
-  #  owner_address
     amount
     activity_type
     coin_type
-  }}
-}}
+  }
+}
 """
 
 data = query_api(query)

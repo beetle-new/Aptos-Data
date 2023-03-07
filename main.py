efb824e472e89aa0045b2374c486589c98ac1e89
beetle <121641549+beetle-new@users.noreply.github.com>
2023-03-07 14:46:44 -0500

    Update main.py
    new changes

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import requests
 import pandas as pd
 import streamlit as st
 
-
 def query_api(query):
     url = "https://indexer.mainnet.aptoslabs.com/v1/graphql"
     headers = {
@@ -16,76 +15,71 @@ def query_api(query):
         return response.json()
     else:
         return None
-
-
-def get_data(owner_address):
-    query = f"""
-    query MyQuery {{
-      coin_activities(
-         where: {{owner_address: {{_eq: "{owner_address}"}}, is_transaction_success: {{_eq: true}}, activity_type: {{_neq: "0x1::aptos_coin::GasFeeEvent"}}}}
-         order_by: {{transaction_timestamp: desc}}
-      ) {{
-        transaction_timestamp
-        amount
-        activity_type
-        coin_type
+# Left column for balance and transaction table
+with st.container():
+      st.header("Enter owner address")
+      owner_address = st.text_input("Owner address", "0xc739507214d0e1bf9795485299d709e00024e92f7c0d055a4c2c39717882bdfd")
+      query_balances = f"""
+      query {{
+        current_coin_balances(
+          where: {{owner_address: {{_eq: "{owner_address}"}}}}
+        ) {{
+          owner_address
+          amount
+          coin_info {{
+            symbol
+          }}
+        }}
       }}
-    }}
-    """
-    data = query_api(query)
-    df = pd.DataFrame(data['data']['coin_activities'])
-    df = df.rename(columns={"transaction_timestamp": "Date",
-                            "amount": "amount"})
+      """
+      data_balances = query_api(query_balances)
+      if data_balances:
+          df_balances = pd.DataFrame(data_balances['data']['current_coin_balances'])
+          df_balances = df_balances.rename(columns={"owner_address": "Owner Address",
+                                                  "amount": "Amount",
+                                                  "coin_info": "Coin Info"})
+          df_balances = pd.concat([df_balances.drop(['Coin Info'], axis=1), df_balances['Coin Info'].apply(pd.Series)], axis=1)
+          df_balances['Amount'] = round(df_balances['Amount'] / 100000000, 2)
+          df_balances['Amount'] = df_balances['Amount'].apply(lambda x: "{:,.2f}".format(x))
+          st.write("Balance Table")
+          st.dataframe(df_balances.style.set_table_styles([{'selector': 'th', 'props': [('background-color', '#2F4F4F'),('color', 'white')]}]).set_properties(**{'font-size': '10pt', 'width': '100%', 'overflow-x': 'auto'}))
 
-    df['amount'] = round(df['amount'] / 100000000,2)
-    df['amount'] = df['amount'].apply(lambda x: "{:,.2f}".format(x))
-    df['coin_type'] = df['coin_type'].str.split("::").str[-1]
-    df['coin_type'] = df['coin_type'].str.rsplit("Event", 1).str[0]
-    df['activity_type'] = df['activity_type'].str.split("::").str[-1]
-    df['activity_type'] = df['activity_type'].str.rsplit("Event", 1).str[0]
-    
-    activity_types = df['activity_type'].nunique()
-    st.write("Total distinct activity types: ", activity_types) 
-    return df
+      query_activities = f"""
+      query {{
+        coin_activities(
+          where: {{owner_address: {{_eq: "{owner_address}"}}, is_transaction_success: {{_eq: true}}}}
+        ) {{
+          transaction_timestamp
+          owner_address
+          amount
+          activity_type
+        }}
+      }}
+      """
+      data_activities = query_api(query_activities)
+      if data_activities:
+          df_activities = pd.DataFrame(data_activities['data']['coin_activities'])
+          df_activities = df_activities.rename(columns={"transaction_timestamp": "Date",
+                                                      "owner_address": "Address",
+                                                      "amount": "Amount"})
+          df_activities['Amount'] = round(df_activities['Amount'] / 100000000, 2)
+          df_activities['Amount'] = df_activities['Amount'].apply(lambda x: "{:,.2f}".format(x))
+          df_activities['activity_type'] = df_activities['activity_type'].str.split("::").str[-1]
+          df_activities['activity_type'] = df_activities['activity_type'].str.rsplit("Event", 1).str[0]
+          st.write("Transaction Table")
+          st.dataframe(df_activities.style.set_table_styles([{'selector': 'th', 'props': [('background-color', '#2F4F4F'),('color', 'white')]}]).set_properties(**{'font-size': '10pt', 'width': '100%', 'overflow-x': 'auto'}))
 
-owner_address = "0xc739507214d0e1bf9795485299d709e00024e92f7c0d055a4c2c39717882bdfd"
-owner_address = st.text_input("Enter an owner address:", value=owner_address)
-
-query = """
-query MyQuery {
-  current_coin_balances(
-    where: {owner_address: {_eq: "%s"}}
-  ) {
-    amount
-    coin_info {
-      symbol
-    }
-  }
-}
-""" % owner_address
-
-if owner_address:
-    data = query_api(query)
-    df = pd.DataFrame(data['data']['current_coin_balances'])
-    df = df.rename(columns={"amount": "amount",
-                            "coin_info": "coin_info"})
-    df = pd.concat([df.drop(['coin_info'], axis=1),df['coin_info'].apply(pd.Series)], axis=1)
-    ### NOT SURE WHY I HAVE TO DIVIDE BY THIS NUMBER TO GET THE CORRECT VALUE ####
-    df['amount'] = round(df['amount'] / 100000000,2)
-    df['amount'] = df['amount'].apply(lambda x: "{:,.2f}".format(x))
-
-if owner_address:
-    st.table(df)
-    st.table(get_data(owner_address))
-
-
-
-data = query_api(query)
-df2 = pd.DataFrame(data['data']['current_coin_balances'])
-df2 = df2.rename(columns={"owner_address": "owner_address",
-                        "amount": "amount",
-                        "coin_info": "coin_info"})
-df2 = pd.concat([df2.drop(['coin_info'], axis=1), df2['coin_info'].apply(pd.Series)], axis=1)
-df2['amount'] = round(df2['amount'] / 100000000, 2)
-df2['amount'] = df2['amount'].apply(lambda x: "{:,.2f}".format(x))
-st.table(df2)
+# Right column for name search
+with st.container():
+  st.header("Search for names in sanctions list")
+  first_name = st.text_input("First Name", "")
+  last_name = st.text_input("Last Name", "")
+  sanctions_df = pd.read_csv('consolidated.csv')
+  sanctions_df = sanctions_df[(sanctions_df['firstName'].str.contains(first_name, case=False)) &
+                              (sanctions_df['lastName'].str.contains(last_name, case=False))]
+  sanctions_table = pd.DataFrame(sanctions_df, columns=['firstName', 'lastName', 'sdnType',
+  'akaList/aka/0/category','akaList/aka/0/lastName', 'akaList/aka/0/firstName', 
+  'akaList/aka/1/category', 'akaList/aka/1/lastName', 'akaList/aka/1/firstName',
+  'dateOfBirthList/dateOfBirthItem/dateOfBirth','placeOfBirthList/placeOfBirthItem/placeOfBirth',  'addressList/address/0/stateOrProvince', 'addressList/address/0/country',
+  'addressList/address/0/city', 'programList/program/','remarks'])
+  st.dataframe(sanctions_table.style.set_table_styles([{'selector': 'th', 'props': [('background-color', '#2F4F4F'),('color', 'white')]}]).set_properties(**{'font-size': '10pt', 'width': '100%', 'overflow-x': 'auto'}))
